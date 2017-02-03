0022e0d29a49eb8c58d5b4fc4273b8dd380d618d
Cavin Dsouza <cavin46016@gmail.com>
2017-02-02 20:01:29 -0500

    Update Problem5.py

--- Problem5.py
+++ Problem5.py
@@ -1,3 +1,6 @@
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
 import math
 from utils import generatePrimes
 
